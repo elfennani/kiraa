116cf4f37e8a1b2f5bf9e0552af74483d05a026a
elfennani <elfennani2002@gmail.com>
2026-06-20 17:48:26 +0100

    Fixed tsconfig build errors.

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     println!("running...");
     loop {
 //         println!("Ping!");
-        sleep(Duration::from_secs(1)).await;
+        tokio::time::sleep(Duration::from_secs(1)).await;
     }
 
     println!("Stopped!");
